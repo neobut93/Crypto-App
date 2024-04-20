7d86ff05dae8b78acc5a20ced79f1e20a1e2342c
vadim <neobut93@gmail.com>
2024-04-20 13:22:41 -0700

    added space to current price

--- app/src/main/java/com/cryptoapp/ui/screens/cryptodetails/components/Details.kt
+++ app/src/main/java/com/cryptoapp/ui/screens/cryptodetails/components/Details.kt
@@ -1,28 +1,20 @@
 package com.cryptoapp.ui.screens.cryptodetails.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.cryptoapp.models.Crypto
 import com.cryptoapp.sample.sampleCrypto
-import com.cryptoapp.utils.PriceFormatting
 
 
 @Composable
@@ -32,26 +24,7 @@ fun CryptoDetails(
 ) {
     LazyColumn(modifier = modifier.padding(start = 8.dp)) {
         item {
-            Text(text = "Current price", fontStyle = FontStyle.Italic)
-        }
-
-        item {
-            Row(verticalAlignment = Alignment.Bottom) {
-                Text(
-                    text = "$" + PriceFormatting.formatCurrentPrice(crypto.current_price),
-                    fontWeight = FontWeight.Bold, fontSize = 45.sp
-                )
-                Text(
-                    text = PriceFormatting.formatPercentageChange(crypto.price_change_percentage_24h) + " %",
-                    fontWeight = FontWeight.Bold, fontSize = 15.sp,
-                    color = if (crypto.price_change_percentage_24h > 0) {
-                        Color(0xFF63B960)
-                    } else {
-                        Color.Red
-                    },
-                    modifier = Modifier.padding(start = 5.dp)
-                )
-            }
+            CurrentData(crypto = crypto)
         }
 
         item {
@@ -60,13 +33,13 @@ fun CryptoDetails(
         }
 
         item {
-            Row(){
+            Row {
                 HoursData(crypto)
                 PerformanceChart(
                     modifier = Modifier
                         .width(230.dp)
                         .height(120.dp)
-                        .padding(start = 20.dp, bottom = 5.dp, top =10.dp, end = 10.dp),
+                        .padding(start = 20.dp, bottom = 5.dp, top = 10.dp, end = 10.dp),
                     startPrice = crypto.current_price.toFloat() - crypto.price_change_24h.toFloat(),
                     highestPrice = crypto.high_24h.toFloat(),
                     lowestPrice = crypto.low_24h.toFloat(),
