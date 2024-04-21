f75dc22786ce8ea3341e2e3f9d1a4449192d790f
vadim <neobut93@gmail.com>
2024-04-20 18:40:37 -0700

    added styles

--- app/src/main/java/com/cryptoapp/ui/screens/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/cryptoapp/ui/screens/welcome/WelcomeScreen.kt
@@ -1,6 +1,7 @@
 package com.cryptoapp.ui.screens.welcome
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,12 +12,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cryptoapp.datastore.PreferencesConstants.WELCOME_KEY
 import com.cryptoapp.datastore.PreferencesManager
@@ -28,7 +33,9 @@ fun CryptoWelcomeScreen(
     val context = LocalContext.current
     val preferencesManager = remember { PreferencesManager(context) }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()
+        .background(MaterialTheme.colorScheme.background)
+    ) {
         CryptoWelcomePage()
         Spacer(modifier = Modifier.height(20.dp))
         Row(
@@ -43,7 +50,10 @@ fun CryptoWelcomeScreen(
                     preferencesManager.saveData(WELCOME_KEY, true)
                     navigate()
                 },
-                shape = RoundedCornerShape(size = 6.dp)
+                shape = RoundedCornerShape(size = 6.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = MaterialTheme.colorScheme.onPrimary
+                )
             ) {
                 Text(text = "Click me")
             }
@@ -52,8 +62,8 @@ fun CryptoWelcomeScreen(
 }
 
 
-//@Preview
-//@Composable
-//fun OnBoardingScreenPreview() {
-//    CryptoWelcomeScreen()
-//}
+@Preview
+@Composable
+fun OnBoardingScreenPreview() {
+    CryptoWelcomeScreen {}
+}
