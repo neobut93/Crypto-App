30156df19828ae1479f393d92f43ca687a85d9f5
vadim <neobut93@gmail.com>
2024-04-24 06:22:39 -0700

    added return loading state test

--- app/src/test/java/com/cryptoapp/ui/screens/cryptolist/CryptoListViewModelTest.kt
+++ app/src/test/java/com/cryptoapp/ui/screens/cryptolist/CryptoListViewModelTest.kt
@@ -1,6 +1,9 @@
 package com.cryptoapp.ui.screens.cryptolist
 
+import app.cash.turbine.test
 import com.cryptoapp.repositories.CryptoRepository
+import com.cryptoapp.ui.screens.cryptodetails.CryptoDetailsState
+import com.cryptoapp.ui.screens.cryptodetails.CryptoDetailsViewModel
 import io.mockk.mockk
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
@@ -30,6 +33,17 @@ class CryptoListViewModelTest {
     fun tearDown() {
         Dispatchers.resetMain()
         mainThreadSurrogate.close()
+    }
+
+    @Test
+    fun `loading state return loading`() = runBlocking {
+         val mockCryptoRepository = mockk<CryptoRepository>(relaxed = true)
+
+        val sut = CryptoListViewModel(mockCryptoRepository)
+
+        sut.uiState.test {
+            assertEquals(CryptoListState.Loading, awaitItem())
+        }
     }
 
     @Test
